ee5793202b69e72c308010b99101d70ce4a490e7
Aaron Rosenberg <aaronrbg@gmail.com>
2018-11-07 04:06:39 +0000

    tweet values are now escaped to fix code injection

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -54,20 +54,24 @@ $(document).ready(function() {
           "created_at": 1461113796368
         }
       ];
+    
+    //this function escapes any possible html or script which could end up in the DOM
+    function escape(text){
+        let escaped = $('<div/>').text(text).html()
+        return escaped;
+    }
 
     function createTweetElement(tweetDataObject){
-        const img = tweetDataObject.user.avatars.small;
-        const name = tweetDataObject.user.name;
-        const handle = tweetDataObject.user.handle;
-        const text = tweetDataObject.content.text;
-        const timestamp = tweetDataObject.created_at;
+        const img = escape(tweetDataObject.user.avatars.small);
+        const name = escape(tweetDataObject.user.name);
+        const handle = escape(tweetDataObject.user.handle);
+        const text = escape(tweetDataObject.content.text);
+        const timestamp = escape(tweetDataObject.created_at);
 
         let $tweet = $('<article>').addClass('tweet');
         $tweet.append(`<header><img src="${img}"><h2>${name}</h2><span class='userID'>${handle}</span></header>`);
         $tweet.append(`<p>${text}</p>`);
-        $tweet.append(`<footer>${timestamp}`);
-        //$tweet.append(`<i class="fas fa-flag"></i>
-        $tweet.append(`</footer></article>`);
+        $tweet.append(`<footer>${timestamp}<i class="fas fa-flag"></i></footer></article>`);
         return $tweet;
     }
 
